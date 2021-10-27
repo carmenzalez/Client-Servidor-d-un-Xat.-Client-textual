a14d4c48055faf096b5d91078d270ae4b5ad2772
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-10-27 16:36:37 +0200

    Update and rename EchoClient.java to MyClient.java

--- MyClient.java
+++ MyClient.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
-public class EchoClient {
+public class MyClient {
 
     public static void main(String[] args) {
         
