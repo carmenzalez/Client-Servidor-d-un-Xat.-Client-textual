29c14158df5a421856e598bc9f8785623a01d833
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-10-27 16:30:28 +0200

    Update EchoServer.java

--- EchoServer.java
+++ EchoServer.java
@@ -8,6 +8,18 @@ import java.io.PrintWriter;
 public class EchoServer {
 
     public static void main(String[] args) {
+        MyServerSocket ss = new MyServerSocket(Integer.parseInt(args[0]));
         
+        while(true) {
+            MySocket s = ss.accept();
+            new Thread() {
+                public void run(){
+                    String line;
+                    while((line = s.readLine()) != null) {
+                        s.println();
+                    }
+                }
+            }.start();
+        }
     }
 }
